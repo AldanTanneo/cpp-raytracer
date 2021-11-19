b6ad1d1ac42a852cf7a22dcc0f1cfbf44f290326
AldanTanneo <aldantanneo@gmail.com>
2021-11-19 17:06:41 +0100

    forward args to build command

--- build.bat
+++ build.bat
@@ -1,4 +1,9 @@
 @echo off
-@echo Building main.exe...
-clang++ -o main.exe src/*.cpp -Isrc/include -Wall -Werror -Wfatal-errors
+IF "%1"=="--release" (
+    @echo Building main.exe [release mode]
+    clang++ -o main.exe src/*.cpp -Isrc/include -Wall -Werror -Wfatal-errors -O3
+) ELSE (
+    @echo Building main.exe [debug mode]
+    clang++ -o main.exe src/*.cpp -Isrc/include -Wall -Werror -Wfatal-errors -O0 %*
+)
 @echo Done!
