54a8322dd924578b81aef9e4119a6719b738de0f
Bastien HUBERT <hbast4@gmail.com>
2021-11-23 11:17:47 +0100

    deleted build.bat

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-IF "%1"=="--release" (
-    @echo Building main.exe [release mode]
-    clang++ -o main.exe src/*.cpp -Isrc/include -Wall -Werror -Wfatal-errors -O3
-) ELSE (
-    @echo Building main.exe [debug mode]
-    clang++ -o main.exe src/*.cpp -Isrc/include -Wall -Werror -Wfatal-errors -O0 %*
-)
-@echo Done!
