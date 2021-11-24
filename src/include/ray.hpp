6e668e6a3b95ecd34a15f12f3897051e5cd16877
AldanTanneo <aldantanneo@gmail.com>
2021-11-24 13:30:02 +0100

    added debugging

--- src/include/ray.hpp
+++ src/include/ray.hpp
@@ -6,13 +6,23 @@
 
 class Ray {
   private:
+    /* The origin of the ray */
     Point3 origin;
+    /* The direction of the ray */
     Vec3 direction;
 
   public:
+    /* Construct a ray from its origin and direction */
     constexpr Ray(Point3 origin, Vec3 direction) noexcept : origin(origin), direction(direction) {}
 
+    /* Compute the position of the ray at time t0 */
     constexpr Point3 at(double t0) noexcept { return origin + t0 * direction; }
+
+    /* Print the ray for debugging purposes */
+    template <class charT, class charTraits = std::char_traits<charT>>
+    inline friend std::basic_ostream<charT, charTraits> & operator<<(std::basic_ostream<charT, charTraits> & os, const Ray & self) {
+        return os << "Ray(" << self.origin << ", " << self.direction << ")";
+    }
 };
 
 #endif
