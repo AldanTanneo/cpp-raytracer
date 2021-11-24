9ade834dcdad2cda37d05b2e8f8bd6ccffe189f6
AldanTanneo <aldantanneo@gmail.com>
2021-11-24 16:24:29 +0100

    camera optimisations & aspect ratio changes

--- src/camera.cpp
+++ src/camera.cpp
@@ -9,18 +9,14 @@ Camera::Camera(Point3 origin, Point3 look_at, Vec3 up_vector, double vertical_fo
     if (vertical_fov < utils::EPSILON || vertical_fov > 180 - utils::EPSILON) {
         throw "Could not build camera: vertical fov must be an angle between 0° and 180°";
     }
-    if (aspect_ratio.value() < utils::EPSILON) {
-        throw "Could not build camera: aspect_ratio must be a positive value";
-    }
 
     Vec3 direction = look_at - origin;
-    double distance_to_focus = direction.norm();
-    if (distance_to_focus < utils::EPSILON) {
+    if (direction.squared_norm() < utils::EPSILON) {
         throw "Could not build camera: the origin and focus points must be different";
     }
 
     // Compute an orthonormal base of the screen space
-    Vec3 x = direction / distance_to_focus;
+    Vec3 x = direction.unit_vector();
     Vec3 z = x.cross(up_vector).unit_vector();
     Vec3 y = z.cross(x); // No need to normalize, it's a cross product between 2 unit vectors
     if (z.squared_norm() < utils::EPSILON) {
@@ -28,11 +24,11 @@ Camera::Camera(Point3 origin, Point3 look_at, Vec3 up_vector, double vertical_fo
     }
 
     // Compute the height and width of the virtual screen
-    double viewport_height = 2 * distance_to_focus * tan(utils::to_radians(vertical_fov) / 2);
+    double viewport_height = 2 * tan(utils::to_radians(vertical_fov) / 2);
     double viewport_width = viewport_height * aspect_ratio.value();
 
     // Final camera setup
     horizontal_vector = viewport_width * z;
     vertical_vector = viewport_height * y;
-    origin_to_bottom_left_corner = direction - horizontal_vector / 2 - vertical_vector / 2;
+    origin_to_bottom_left_corner = x - horizontal_vector / 2 - vertical_vector / 2;
 }
--- src/include/camera.hpp
+++ src/include/camera.hpp
@@ -1,6 +1,7 @@
 #ifndef CAMERA_HPP
 #define CAMERA_HPP
 
+#include <cmath>
 #include <numeric>
 
 // From src/include
@@ -11,13 +12,21 @@
 struct AspectRatio {
   private:
     /* The wrapped aspect ratio */
-    long double aspect_ratio;
+    double aspect_ratio;
 
   public:
-    /* Construct an aspect ratio from two integer */
-    constexpr AspectRatio(unsigned int width, unsigned int height) noexcept : aspect_ratio((long double)width / (long double)height) {}
-    /* Construct an aspect ratio from a long double */
-    constexpr AspectRatio(long double value) noexcept : aspect_ratio(value) {}
+    /* Construct an aspect ratio from two integers */
+    constexpr AspectRatio(unsigned int width, unsigned int height) : aspect_ratio((double)width / (double)height) {
+        if (aspect_ratio < utils::EPSILON || !std::isfinite(aspect_ratio)) {
+            throw "Could not construct AspectRatio: value must be positive";
+        }
+    }
+    /* Construct an aspect ratio from a double */
+    constexpr AspectRatio(double value) : aspect_ratio(value) {
+        if (aspect_ratio < utils::EPSILON || !std::isfinite(aspect_ratio)) {
+            throw "Could not construct AspectRatio: value must be positive";
+        }
+    }
 
     /* Get the inner value */
     constexpr long double value() noexcept { return aspect_ratio; }
