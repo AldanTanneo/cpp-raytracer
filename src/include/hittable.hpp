ac22fdecc03564a5b1693ad90106d26f4a685a41
AldanTanneo <aldantanneo@gmail.com>
2021-11-27 00:57:22 +0100

    face normal orientation

--- src/include/hittable.hpp
+++ src/include/hittable.hpp
@@ -1,6 +1,7 @@
 #ifndef HITTABLE_HPP
 #define HITTABLE_HPP
 
+#include <iterator>
 #include <memory>
 #include <vector>
 
@@ -15,11 +16,19 @@ struct HitRecord {
     Point3 hit_point;
     /* The normal of the object surface at the intersection point */
     Vec3 surface_normal;
+    /* Determines if the surface is hit from the front or the back */
+    bool front_face;
     /* The material of the object surface */
     // Material mat;
 
     /* Construct an empty hit record */
-    constexpr HitRecord() noexcept : time(0.0), hit_point(point3::ZEROS), surface_normal(point3::ZEROS) {}
+    constexpr HitRecord() noexcept : time(0.0), hit_point(point3::ZEROS), surface_normal(point3::ZEROS), front_face(false) {}
+
+    /* Set the correct orientation of the normal */
+    constexpr void set_face_normal(const Ray & r, const Vec3 & outward_normal) noexcept {
+        front_face = r.direction.dot(outward_normal) < 0;
+        surface_normal = front_face ? outward_normal : -outward_normal;
+    }
 };
 
 /* Abstract class of a hittable object */
@@ -39,21 +48,31 @@ class HittableList : public Hittable {
     std::vector<std::unique_ptr<Hittable>> objects;
 
   public:
+    /* Construct an empty hittable list */
     inline HittableList() noexcept : objects(std::vector<std::unique_ptr<Hittable>>()) {}
 
-    const std::vector<std::unique_ptr<Hittable>> & iter() const noexcept {
-        return objects;
-    }
-
+    /* Add an object inheriting from the Hittable class */
     template <class T>
     void add(T object) noexcept {
         objects.push_back(std::make_unique<T>(object));
     }
 
+    /* Indexing operator overloading */
     const std::unique_ptr<Hittable> & operator[](int index) const {
         return objects[index];
     }
 
+    /* const iterator begin definition */
+    inline std::vector<std::unique_ptr<Hittable>>::const_iterator begin() const noexcept {
+        return objects.begin();
+    }
+
+    /* Const iterator end definition */
+    inline std::vector<std::unique_ptr<Hittable>>::const_iterator end() const noexcept {
+        return objects.end();
+    }
+
+    /* Hit method override */
     virtual bool hit(const Ray & ray_in, double tmin, double tmax, HitRecord & hit_record) const noexcept override;
 };
 
