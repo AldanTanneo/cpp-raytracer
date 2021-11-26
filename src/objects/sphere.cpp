90b19888599859b6ad6e22d5443821c7ddc09d13
AldanTanneo <aldantanneo@gmail.com>
2021-11-26 21:07:31 +0100

    finished sphere intersection code

--- src/objects/sphere.cpp
+++ src/objects/sphere.cpp
@@ -2,22 +2,29 @@
 #include <objects/sphere.hpp>
 #include <utils/vec3.hpp>
 
-bool Sphere::hit(Ray ray_in, double tmin, double tmax, HitRecord & hit_record) const {
+bool Sphere::hit(const Ray & ray_in, double tmin, double tmax, HitRecord & hit_record) const {
     // sovling ( ray_origin + t * ray_direction - sphere_centre )^2 = sphere_radius^2
-    double a = ray_in.get_direction().dot(ray_in.get_direction());
-    double b = 2. * ray_in.get_direction().dot(ray_in.get_origin() - centre);
-    double c = (ray_in.get_origin() - centre).dot(ray_in.get_origin() - centre);
-    double delta = b * b - 4. * a * c;
+    double a = ray_in.direction.squared_norm();
+    double b = 2.0 * ray_in.direction.dot(ray_in.origin - centre);
+    double c = (ray_in.origin - centre).squared_norm() - radius * radius;
+    double delta = b * b - 4.0 * a * c;
     if (delta < 0) { // sphere not hit
         return false;
     } else if (delta == 0) { // sphere hit in 1 point
-        double t = -b / (2. * a);
-        hit_record.hit_point = ray_in.get_origin() + t * ray_in.get_direction();
+        hit_record.time = -b / (2.0 * a);
+        hit_record.hit_point = ray_in.at(hit_record.time);
+        hit_record.surface_normal = (hit_record.hit_point - centre) / radius;
         return true;
     } else { // sphere hit in 2 points
-        double t = (-b - sqrt(delta)) / (2. * a);
-        t = (t > utils::EPSILON) ? t : (-b - sqrt(delta)) / (2. * a); // smallest positive solution
-        hit_record.hit_point = ray_in.get_origin() + t * ray_in.get_direction();
-        return true;
+        double t = (-b - sqrt(delta)) / (2.0 * a);
+        t = (t > tmin) ? t : (-b + sqrt(delta)) / (2.0 * a); // smallest positive solution
+        if (t > tmax) {
+            return false;
+        } else {
+            hit_record.time = t;
+            hit_record.hit_point = ray_in.at(hit_record.time);
+            hit_record.surface_normal = (hit_record.hit_point - centre) / radius;
+            return true;
+        }
     }
 }
