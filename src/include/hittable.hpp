7c0f8cf53811936a0d89e0d15782baaca8bb684f
AldanTanneo <aldantanneo@gmail.com>
2021-11-26 17:08:44 +0100

    made Hittable::hit const

--- src/include/hittable.hpp
+++ src/include/hittable.hpp
@@ -9,7 +9,7 @@ class Hittable {
     // get_materiau
 
     /* define behaviour of ray when hitting hittable object */
-    virtual bool hit(Ray ray_in, double tmin, double tmax, Ray & ray_out) = 0;
+    virtual bool hit(Ray ray_in, double tmin, double tmax, Ray & ray_out) const = 0;
 };
 
 #endif
