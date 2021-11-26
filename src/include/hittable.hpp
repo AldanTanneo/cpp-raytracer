0537dbff67a4124e2f4f5e30db87a28f3775127a
AldanTanneo <aldantanneo@gmail.com>
2021-11-26 17:54:06 +0100

    modified hittable abstract class

--- src/include/hittable.hpp
+++ src/include/hittable.hpp
@@ -3,13 +3,24 @@
 
 // From src/include
 #include <ray.hpp>
+#include <utils/vec3.hpp>
+
+/* The returned structure if a ray hits an object */
+struct HitRecord {
+    /* The point of intersection between the ray and the object */
+    Point3 hit_point;
+    /* The normal of the object surface at the intersection point */
+    Vec3 surface_normal;
+    /* The material of the object surface */
+    // Material mat;
+};
 
 class Hittable {
   public:
     // get_materiau
 
     /* define behaviour of ray when hitting hittable object */
-    virtual bool hit(Ray ray_in, double tmin, double tmax, Ray & ray_out) const = 0;
+    virtual bool hit(Ray ray_in, double tmin, double tmax, HitRecord & hit_record) const = 0;
 };
 
 #endif
---
+++ src/objects/sphere.cpp
@@ -0,0 +1,8 @@
+#include <hittable.hpp>
+#include <objects/sphere.hpp>
+#include <utils/vec3.hpp>
+
+bool Sphere::hit(Ray ray_in, double tmin, double tmax, HitRecord & hit_record) const {
+    // TODO
+    return false;
+}
