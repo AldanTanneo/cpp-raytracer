afbb90b7cb3cd1e6db90775514da2c63b0d85ba6
AldanTanneo <aldantanneo@gmail.com>
2021-11-26 21:39:57 +0100

    added more random gen functions

--- src/include/utils/rng.hpp
+++ src/include/utils/rng.hpp
@@ -1,9 +1,15 @@
 #ifndef RNG_HPP
 #define RNG_HPP
 
+#include <stdint.h>
+
 namespace rng {
 /* Return a random double in the range [0, 1) */
 double gen();
+/* Return a random 64 bit integer */
+uint64_t gen_u64();
+/* Return a random 32 bit integer */
+uint32_t gen_u32();
 } // namespace rng
 
 #endif
--- src/utils/rng.cpp
+++ src/utils/rng.cpp
@@ -1,11 +1,15 @@
 #include <stdint.h>
 #include <utils/rng.hpp>
 
+/* Private random number generator based on SplitMix64. */
 struct FastRng {
+    /* The inner 64-bit value */
     uint64_t value;
 
+    /* Construct a new instance from a seed */
     constexpr FastRng(const uint64_t seed) noexcept : value(seed) {}
 
+    /* Fetch the next 64-bit integer */
     constexpr uint64_t next_u64() noexcept {
         uint64_t z = value + 0x9E3779B97F4A7C15ULL;
         value = z;
@@ -14,6 +18,7 @@ struct FastRng {
         return z ^ (z >> 31);
     }
 
+    /* Fetch the next 64-bit integer and cast it to a double in the [0, 1) range */
     inline double next_f64() noexcept {
         uint64_t i = next_u64();
         constexpr uint64_t mask1 = 0x3FF0000000000000ULL;
@@ -23,10 +28,20 @@ struct FastRng {
     }
 };
 
+/* Global, thread-local RNG instance */
 thread_local FastRng glob_rng(0x193a6754ULL);
 
+/* Export a single function to the external library, users cannot see the inside of the beast */
 namespace rng {
 double gen() {
     return glob_rng.next_f64();
+}
+
+uint64_t gen_u64() {
+    return glob_rng.next_u64();
+}
+
+uint32_t gen_u32() {
+    return glob_rng.next_u64() >> 32;
 }
 } // namespace rng
