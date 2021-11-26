7f1313983d9bea04c1063dc6750fc0d7b89f31ab
Bastien HUBERT <hbast4@gmail.com>
2021-11-26 19:04:21 +0100

    implemented Sphere::hit

--- src/objects/sphere.cpp
+++ src/objects/sphere.cpp
@@ -3,6 +3,21 @@
 #include <utils/vec3.hpp>
 
 bool Sphere::hit(Ray ray_in, double tmin, double tmax, HitRecord & hit_record) const {
-    // TODO
-    return false;
+    // sovling ( ray_origin + t * ray_direction - sphere_centre )^2 = sphere_radius^2
+    double a = ray_in.get_direction().dot(ray_in.get_direction());
+    double b = 2. * ray_in.get_direction().dot(ray_in.get_origin() - centre);
+    double c = (ray_in.get_origin() - centre).dot(ray_in.get_origin() - centre);
+    double delta = b * b - 4. * a * c;
+    if (delta < 0) { // sphere not hit
+        return false;
+    } else if (delta == 0) { // sphere hit in 1 point
+        double t = -b / (2. * a);
+        hit_record.hit_point = ray_in.get_origin() + t * ray_in.get_direction();
+        return true;
+    } else { // sphere hit in 2 points
+        double t = (-b - sqrt(delta)) / (2. * a);
+        t = (t > utils::EPSILON) ? t : (-b - sqrt(delta)) / (2. * a); // smallest positive solution
+        hit_record.hit_point = ray_in.get_origin() + t * ray_in.get_direction();
+        return true;
+    }
 }
