8a5b72fad010d3509e882e706d9b07660c49d63f
AldanTanneo <aldantanneo@gmail.com>
2021-11-26 21:07:42 +0100

    first render!!!!!!!!

--- src/main.cpp
+++ src/main.cpp
@@ -4,66 +4,54 @@
 // From src/include
 #include <camera.hpp>
 #include <main.hpp>
+#include <objects/sphere.hpp>
 #include <utils.hpp>
+#include <utils/rng.hpp>
 
-
-void tests() {
-    Camera cam(vec3::ZEROS, vec3::X, vec3::Y, 90, AspectRatio(1));
-
-    DEBUG(cam.get_ray(0.5, 0.5), "looking right in the middle of the screen: direction = X");
-    DEBUG(cam.get_ray(0, 0), "bottom left corner of the screen");
-    DEBUG(cam.get_ray(1, 1), "top right corner of the screen");
-    newline();
-
-    Vec3 u(1, -1, 1);
-    DEBUG(u);
-    DEBUG(u.reflect(vec3::Y), "Reflect u along the (x, z) plane, defined by its normal y");
-    DEBUG(u + vec3::Y);
-    newline();
-
-    Colour test = 0.503 * colour::RED + 0.7 * colour::BLUE + 0.3 * colour::GREEN;
-    DEBUG(test);
-    DEBUG(test.to_hex());
-    std::cout << "0x" << std::hex << test.to_hex() << std::endl;
-    DEBUG("test");
-    newline();
+void test_3d_vectors() {
+    Vec3 u(0, 1, 0);
+    Vec3 v(sqrt(2) / 2, 0, sqrt(2) / 2);
+    std::cout << "u . v = " << u.dot(v) << std::endl;
+    std::cout << "u x v = " << u.cross(v) << std::endl;
+    std::cout << 3 * vec3::ONES << std::endl;
 }
 
 int main(int argc, char * argv[]) {
-    tests();
 
-    int width = 200;
-    int height = 200;
+    int height = 1080;
+    int width = (height * 16) / 9;
 
     image::Image img(width, height);
 
+    Camera cam(point3::ZEROS, -point3::Z, vec3::Y, 90, AspectRatio(16, 9));
+
+    Sphere ball(-point3::Z, 0.5);
+    Sphere ball2(Vec3(0.2, 0.2, -1.2), 0.5);
+    HitRecord h;
+
     for (int j = height - 1; j >= 0; j--) {
         for (int i = 0; i < width; i++) {
-            double r = double(i) / double(width - 1);
-            double g = double(j) / double(height - 1);
-            double b = (1.0 - r) * (1.0 - g);
+            double u = double(i) / double(width - 1);
+            double v = double(j) / double(height - 1);
+            Ray r = cam.get_ray(u, v);
+            bool hit = false;
+            double tmin = utils::EPSILON;
+            double tmax = utils::INF;
+            if (ball.hit(r, tmin, tmax, h)) {
+                hit = true;
+                tmax = h.time;
+            }
+            hit = ball2.hit(r, tmin, tmax, h) ? true : hit;
 
-            img.push(r, g, b);
+            if (hit) {
+                img.push(0.5 * Colour(h.surface_normal + 1));
+            } else {
+                img.push(colour::BLACK);
+            }
         }
     }
 
     img.save("image.ppm");
-
-    image::Image img2 = image::Image::load("image.ppm");
-
-    DEBUG(img.get_width());
-    DEBUG(img.get_height());
-    newline();
-
-    DEBUG(img2.get_width());
-    DEBUG(img2.get_height());
-    newline();
-
-    std::cout.setf(std::ios_base::boolalpha);
-    DEBUG(img == img2);
-    std::cout.unsetf(std::ios_base::boolalpha);
-
-    img2.save("image2.ppm");
 
     return EXIT_SUCCESS;
 }
