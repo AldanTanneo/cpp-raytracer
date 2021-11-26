5c9d1458ccadd6997e3b5bd3eb8b5356bb93529c
AldanTanneo <aldantanneo@gmail.com>
2021-11-27 00:04:03 +0100

    added hittable list

---
+++ src/hittable.cpp
@@ -0,0 +1,17 @@
+#include <memory>
+
+// From src/include
+#include <hittable.hpp>
+#include <ray.hpp>
+#include <utils/vec3.hpp>
+
+bool HittableList::hit(const Ray & ray_in, double tmin, double tmax, HitRecord & hit_record) const noexcept {
+    bool hit = false;
+    for (const std::unique_ptr<Hittable> & obj : objects) {
+        if (obj->hit(ray_in, tmin, tmax, hit_record)) {
+            hit = true;
+            tmax = hit_record.time;
+        }
+    }
+    return hit;
+}
