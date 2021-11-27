278d5e2b90705967da36a92bfaee325d75f1c0d0
AldanTanneo <aldantanneo@gmail.com>
2021-11-27 05:04:08 +0100

    added diffuse material

---
+++ src/materials/diffuse.cpp
@@ -0,0 +1,15 @@
+
+// From src/include
+#include <hittable.hpp>
+#include <materials/diffuse.hpp>
+#include <utils.hpp>
+
+bool Diffuse::scatter(const Ray & ray_in, const HitRecord & hit_record, Ray & ray_out, Colour & attenuation) const noexcept {
+    ray_out.origin = hit_record.hit_point;
+    ray_out.direction = hit_record.surface_normal + Vec3::random_in_unit_sphere();
+    if (ray_out.direction.near_zero()) {
+        ray_out.direction = hit_record.surface_normal;
+    }
+    attenuation = albedo;
+    return true;
+}
