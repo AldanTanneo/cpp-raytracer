97e9aa50bac6801a577e24c5e16295187a92617e
AldanTanneo <aldantanneo@gmail.com>
2021-11-27 03:49:12 +0100

    antialiasing!

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,11 @@
 
 // From src/include
 #include <camera.hpp>
+#include <image.hpp>
 #include <main.hpp>
 #include <objects/sphere.hpp>
 #include <utils.hpp>
-#include <utils/rng.hpp>
+
 
 void test_3d_vectors() {
     Vec3 u(0, 1, 0);
@@ -19,34 +20,44 @@ void test_3d_vectors() {
 
 int main(int argc, char * argv[]) {
 
-    int height = 720;
+    int height = 1080;
     int width = (height * 16) / 9;
 
     image::Image img(width, height);
 
     Camera cam(point3::ZEROS, -point3::Z, vec3::Y, 90, AspectRatio(16, 9));
 
+    /* Define scene objects */
     Sphere ball(-point3::Z, 0.5);
     Sphere ball2(Vec3(0.2, 0.2, -1.2), 0.5);
     Sphere ground(Vec3(0, -100, 0), 99);
+    /* Create world and add objects to it */
     HittableList world;
     world.add(ball);
     world.add(ball2);
     world.add(ground);
-    HitRecord h;
 
-    for (int j = height - 1; j >= 0; j--) {
-        for (int i = 0; i < width; i++) {
-            double u = double(i) / double(width - 1);
-            double v = double(j) / double(height - 1);
-            Ray r = cam.get_ray(u, v);
+    uint64_t spp = 100;
 
-            if (world.hit(r, utils::EPSILON, utils::INF, h)) {
-                img.push(0.5 * Colour(h.surface_normal + 1));
-            } else {
-                img.push(0.8 * colour::WHITE + 0.2 * colour::CYAN);
+    for (int64_t j = height - 1; j >= 0; j--) {
+        for (int64_t i = 0; i < width; i++) {
+            Colour c;
+            for (int k = 0; k < spp; k++) {
+                double u = (double(i) + rng::gen()) / double(width - 1);
+                double v = (double(j) + rng::gen()) / double(height - 1);
+                Ray r = cam.get_ray(u, v);
+                HitRecord h;
+
+                if (world.hit(r, utils::EPSILON, utils::INF, h)) {
+                    c += 0.5 * Colour(h.surface_normal + 1);
+                } else {
+                    c += 0.8 * colour::WHITE + 0.2 * colour::CYAN;
+                }
             }
+            img.push(c / double(spp));
         }
+        if (!(j % 50))
+            DEBUG(j);
     }
 
     img.save("image.ppm");
