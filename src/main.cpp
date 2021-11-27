a18f935b24bca724de9b38755c525055c5f56b2e
AldanTanneo <aldantanneo@gmail.com>
2021-11-27 17:20:29 +0100

    testing materials

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <cmath>
+#include <ctime>
 #include <iostream>
 #include <memory>
 
 // From src/include
 #include <camera.hpp>
+#include <materials/diffuse.hpp>
 #include <objects/sphere.hpp>
+#include <stdint.h>
+#include <stdlib.h>
 #include <utils.hpp>
 #include <utils/image.hpp>
 
@@ -12,48 +16,64 @@
 #define ERROR_HANDLER_MACRO
 #include <common.hpp>
 
-int main(int argc, char * argv[]) {
+int main(int argc, char * noalias argv[]) {
 
-    const int height = 1080;
-    const int width = (height * 16) / 9;
+    const AspectRatio aspect_ratio(3, 2);
+    const uint32_t height = 500;
+    const uint32_t width = height * aspect_ratio.value();
 
     image::Image img(width, height);
 
-    Camera cam(point3::ZEROS, -point3::Z, vec3::Y, 90, AspectRatio(16, 9));
+    Camera cam(0.5 * point3::Z, -point3::Z, vec3::Y, 60, aspect_ratio);
+
+    std::shared_ptr<Diffuse> grey = std::make_shared<Diffuse>(Diffuse(0.5 * colour::WHITE));
+    std::shared_ptr<Diffuse> red = std::make_shared<Diffuse>(Diffuse(0.75 * colour::RED));
+    std::shared_ptr<Diffuse> purple = std::make_shared<Diffuse>(Diffuse(Colour(0x800080)));
 
     /* Define scene objects */
-    Sphere ball(-point3::Z, 0.5);
-    Sphere ball2(Vec3(0.2, 0.2, -1.2), 0.5);
-    Sphere ground(Vec3(0, -100, 0), 99);
+    Sphere ball(-point3::Z, 0.5, grey);
+    Sphere ball2(Vec3(0.5, 0.1, -1.5), 0.5, red);
+    Sphere ground(Vec3(0, -100, 0), 99.5, purple);
+
     /* Create world and add objects to it */
     HittableList world;
     world.add(ball);
     world.add(ball2);
     world.add(ground);
 
-    constexpr int spp = 64;
+    constexpr int spp = 100;
     constexpr double scale = 1.0 / double(spp);
+    constexpr int max_bounces = 10;
 
-    for (int j = height - 1; j >= 0; j--) {
-        for (int i = 0; i < width; i++) {
+    constexpr int height_by_ten = height / 10;
+
+    for (int j = height; j != 0; j--) {
+        for (int i = 0; i != width; i++) {
             Colour c;
             for (int k = 0; k < spp; k++) {
                 double u = (double(i) + rng::gen()) / double(width - 1);
-                double v = (double(j) + rng::gen()) / double(height - 1);
+                double v = (double(j - 1) + rng::gen()) / double(height - 1);
                 Ray r = cam.get_ray(u, v);
+                Colour ray_color = 0.75 * colour::WHITE;
                 HitRecord h;
-
-                if (world.hit(r, utils::EPSILON, utils::INF, h)) {
-                    c += 0.5 * Colour(h.surface_normal + 1);
-                } else {
-                    c += 0.8 * colour::WHITE + 0.2 * colour::CYAN;
+                int iter = 0;
+                while (iter < max_bounces) {
+                    if (world.hit(r, utils::EPSILON, utils::INF, h)) {
+                        iter++;
+                        h.material->scatter(h, r, ray_color);
+                    } else {
+                        break;
+                    }
                 }
+
+                c += ray_color;
             }
             img.push(c * scale);
         }
-        if (!(j % 50))
-            DEBUG(j);
+        if (!(j % height_by_ten))
+            std::cout << 100 * (height - j) / height << "%" << std::endl;
     }
+    std::cout << "100% complete" << std::endl;
 
     img.save("image.ppm");
 
