798c4bf2f3fd02853891848ec79bc36f686e8d65
AldanTanneo <aldantanneo@gmail.com>
2021-11-29 21:38:56 +0100

    optimized intersection code

--- src/objects/sphere.cpp
+++ src/objects/sphere.cpp
@@ -3,33 +3,33 @@
 #include <objects/sphere.hpp>
 #include <utils/vec3.hpp>
 
-bool Sphere::hit(const Ray & ray_in,
+bool Sphere::hit(const Ray & ray,
                  double tmin,
                  double tmax,
                  HitRecord & hit_record) const noexcept {
     // solving (origin + t * dir - centre)^2 = R^2
-    double a = ray_in.direction.squared_norm();
-    double b = 2.0 * ray_in.direction.dot(ray_in.origin - centre);
-    double c = (ray_in.origin - centre).squared_norm() - radius * radius;
-    double delta = b * b - 4.0 * a * c;
+    const double a = ray.direction.squared_norm();
+    const double b = ray.direction.dot(ray.origin - centre);
+    const double c = (ray.origin - centre).squared_norm() - radius * radius;
+    const double delta = b * b - a * c;
 
     double time = tmin;
 
-    if (delta == 0) { // sphere hit in 1 point
-        time = -b / (2.0 * a);
-    } else { // sphere hit in 2 points
-        time = (-b - sqrt(delta)) / (2.0 * a);
+    if (delta >= 0) { // sphere hit
+        const double sqrt_delta = sqrt(delta);
+        time = (-b - sqrt_delta) / a;
+
         if (time < tmin) {
-            time = (-b + sqrt(delta)) / (2.0 * a); // smallest positive solution
+            time = (-b + sqrt_delta) / a; // smallest positive solution
         }
     }
 
     if (tmin < time && time < tmax) {
         // There was a hit in the time window
         hit_record.time = time;
-        hit_record.hit_point = ray_in.at(time);
+        hit_record.hit_point = ray.at(time);
         Vec3 outward_normal = (hit_record.hit_point - centre) / radius;
-        hit_record.set_face_normal(ray_in, outward_normal);
+        hit_record.set_face_normal(ray, outward_normal);
         hit_record.material = material;
         return true;
     } else {
