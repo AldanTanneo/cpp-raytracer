74099c24b29d7fb67a0b5fb66a1163c21bc1697d
AldanTanneo <aldantanneo@gmail.com>
2021-12-03 00:47:52 +0100

    testing new scenes

--- src/main.cpp
+++ src/main.cpp
@@ -19,50 +19,58 @@
 #include <materials/emissive.hpp>
 #include <materials/metal.hpp>
 #include <materials/plastic.hpp>
+#include <objects/cylinder.hpp>
 #include <objects/sphere.hpp>
 #include <utils/image.hpp>
 #include <utils/progress_bar.hpp>
 
 constexpr AspectRatio aspect_ratio(16, 9);
-constexpr size_t height = 720;
+constexpr size_t height = 1080;
 constexpr size_t width = height * aspect_ratio.value();
 constexpr double height_scale = 1.0 / double(height - 1);
 constexpr double width_scale = 1.0 / double(width - 1);
 
-constexpr int spp = 200;
+constexpr int spp = 1000;
 constexpr double colour_scale = 1.0 / double(spp);
-constexpr int max_bounces = 10;
+constexpr int max_bounces = 15;
 
 /* In-processing kernel parameters */
-constexpr double processing_kernel_weight = 2;
+constexpr double processing_kernel_weight = 1;
 constexpr double processing_kernel_offset =
     (utils::const_sqrt(1 + processing_kernel_weight) - 1) / 2;
 constexpr double processing_kernel_min = 0.0 - processing_kernel_offset;
 constexpr double processing_kernel_max = 1.0 + processing_kernel_offset;
 
-const Camera
-    cam(Vec3(-0.3, 1, 2), Vec3(-0.25, 0.5, -1.5), vec3::Y, 30, aspect_ratio);
+const Camera cam(Vec3(1, 1.4, 3.4), Vec3(0, 0.4, 0), vec3::Y, 30, aspect_ratio);
 
 /* Define scene materials */
-const Plastic blue = Plastic(Colour(0x0015D7), 1.5);
-const BlackBody light = BlackBody(colour::CYAN + 0.05 * colour::MAGENTA, 3.0);
-const Plastic ground_material = Plastic(colour::WHITE);
+const Dielectric glass = Dielectric(Colour(0.9 * colour::WHITE), 1.2);
+const Diffuse ground_material = Diffuse(0.3 * colour::WHITE);
+const Plastic pen_material = Plastic(0.7 * colour::WHITE, 0.05);
 
 /* Define scene objects */
-const Sphere ball(Vec3(0, 0.5, -1.0), 0.25, light);
-const Sphere ball2(Vec3(-0.5, 0.5, -2.0), 0.5, blue);
-const Sphere ground(Vec3(0, -100, 0), 100, ground_material);
+const Sphere ground(Point3(0, -2000, 0), 2000, ground_material);
+const Cylinder cyl(Point3(0, 0, 0), vec3::Y, 0.5, 0.65, glass);
+const Cylinder pen(Point3(0.75, 1, 0), Vec3(-1, -1, 0), 0.05, 3, pen_material);
+const Cylinder test(Point3(-0.7, 0, 0.7), vec3::Y, 0.2, 1, pen_material);
+
+const std::vector<GlobalIllumination> global_light = {
+    GlobalIllumination(
+        Colour(0.8, 0, 0.8), LightType::Infinite, Vec3(-1, -0.05, 1)),
+    GlobalIllumination(2.0 * colour::BLUE, LightType::Point, Vec3(1, 1, 1)),
+    GlobalIllumination(0.05 * colour::WHITE)};
 
 int main(int argc, char * argv[]) {
-    DEBUG(processing_kernel_offset);
     /* Initialize the RNG */
     rng::seed(time(0));
 
     /* Create world and add objects to it */
     HittableList world;
-    world.add(ball);
-    world.add(ball2);
+    // world.add(ball);
     world.add(ground);
+    world.add(cyl);
+    world.add(pen);
+    world.add(test);
 
     /* Create a black image to fill with pixels */
     image::Image img = image::Image::black(width, height);
@@ -72,21 +80,22 @@ int main(int argc, char * argv[]) {
 
     pb.start(term_colours::CYAN);
 
-#pragma omp parallel for default(none) shared(cam, img, pb, world)             \
-    schedule(dynamic)
+#pragma omp parallel for default(none)                                         \
+    shared(cam, img, pb, world, global_light) schedule(dynamic)
     for (size_t index = 0; index < width * height; ++index) {
         Colour c;
         const size_t i = index % width;
         const size_t j = height - 1 - (index / width);
-        double u, v;
         for (int k = 0; k < spp; ++k) {
-            u = (static_cast<double>(i)
+            double u =
+                (static_cast<double>(i)
                  + rng::gen(processing_kernel_min, processing_kernel_max))
                 * width_scale;
-            v = (static_cast<double>(j)
+            double v =
+                (static_cast<double>(j)
                  + rng::gen(processing_kernel_min, processing_kernel_max))
                 * height_scale;
-            c += cam.cast_ray(world, 0.1 * colour::WHITE, max_bounces, u, v);
+            c += cam.cast_ray(world, global_light, max_bounces, u, v);
         }
         img[index] += c * colour_scale;
         pb.advance();
@@ -96,11 +105,13 @@ int main(int argc, char * argv[]) {
 
     std::cout << "Applying blur filter..." << std::endl;
 
-    img.gaussian_blur();
+    /*img.gaussian_blur();*/
 
-    std::cout << "Saving image..." << std::endl;
+    std::cout << "Saving image...";
 
     img.save("image.ppm");
+
+    std::cout << " Done!" << std::endl;
 
     return EXIT_SUCCESS;
 }
