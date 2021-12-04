f83be16cadfa7c78cb754f97fadb7bd3033b6b49
AldanTanneo <aldantanneo@gmail.com>
2021-12-04 21:01:25 +0100

    corrected ray/triangle intersection algorithm

--- src/objects/triangle.cpp
+++ src/objects/triangle.cpp
@@ -9,38 +9,37 @@ bool Triangle::hit(const Ray & ray,
                    double tmin,
                    double tmax,
                    HitRecord & hit_record) const noexcept {
-    // Test if ray's direction is (almost) colinear to the triangle ?
-    const double delta = ray.direction.dot(normal);
-    if (fabs(delta) < utils::EPSILON) {
+    // Test if the ray's direction is colinear to the triangle
+    const double determinant = ray.direction.dot(normal);
+    if (fabs(determinant) < utils::EPSILON) {
+        return false;
+    }
+    const double scale = 1.0 / determinant;
+    const Point3 origin_to_vertex = vertex - ray.origin;
+
+    // Finding the time of intersection
+    const double time = scale * origin_to_vertex.dot(normal);
+    if (time < tmin || tmax < time) {
         return false;
     }
 
-    // Finding the moment of impact
-    double time = (vertex - ray.origin).dot(normal) / delta;
+    // Test if the intersection point is in the triangle (using barycentric
+    // coordinates lambda, mu)
+    const Vec3 direction_cross_origin_to_vertex =
+        ray.direction.cross(origin_to_vertex);
 
-    // Test if is negative (behind the camera) or not in the time window
-    if (time < tmin || time > tmax) {
+    const double lambda = scale * direction_cross_origin_to_vertex.dot(edge1);
+    if (lambda < 0.0) {
         return false;
     }
 
-    // Test if I is in the triangle (using barycentric coordinates w1, w2)
-    const double determinant = edge2.cross(edge1).z;
-    const Point3 intersection_point = ray.at(time);
-    const Vec3 vertex_to_intersection = intersection_point - vertex;
-
-    const double lambda = vertex_to_intersection.cross(edge1).z / determinant;
-    if (lambda < 0) {
+    const double mu = -scale * direction_cross_origin_to_vertex.dot(edge2);
+    if (mu < 0.0 || lambda + mu > 1.0) {
         return false;
     }
 
-    const double mu = -vertex_to_intersection.cross(edge2).z / determinant;
-    if (mu < 0 || lambda + mu > 1) {
-        return false;
-    }
-
-    // Else, there was a hit in the time window
     hit_record.time = time;
-    hit_record.hit_point = intersection_point;
+    hit_record.hit_point = ray.at(time);
     Vec3 outward_normal = normal;
     hit_record.set_face_normal(ray, outward_normal);
     hit_record.material = material;
