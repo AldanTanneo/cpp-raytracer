6387c29b3a57b1024bc19b1657f81f6ba03664f0
AldanTanneo <aldantanneo@gmail.com>
2021-12-10 19:38:14 +0100

    added microfacet model

--- src/include/materials/diffuse.hpp
+++ src/include/materials/diffuse.hpp
@@ -5,15 +5,55 @@
 #include <ray.hpp>
 #include <utils/vec3.hpp>
 
-// Diffuse material: randomly scatters around the surface normal
-class Diffuse : public Material {
+// Lambertian material: randomly scatters around the surface normal
+class Lambertian : public Material {
 private:
     // The colour of the diffuse material
-    Colour albedo;
+    const Colour albedo;
 
 public:
     // Construct a new diffuse material
-    constexpr Diffuse(Colour albedo) noexcept : albedo(albedo) {}
+    constexpr Lambertian(Colour albedo) noexcept : albedo(albedo) {}
+
+    // Virtual function override
+    virtual ScatterType
+    scatter(const HitRecord & hit_record,
+            const Ray & ray_in,
+            ScatterRecord & scatter) const noexcept override;
+
+    // Virtual function override
+    virtual double
+    scattering_pdf(const HitRecord & hit_record,
+                   const Ray & scattered_ray) const noexcept override;
+};
+
+// Oren-Nayar microfacet model
+// (https://pbr-book.org/3ed-2018/Reflection_Models/Microfacet_Models#OrenndashNayarDiffuseReflection)
+class MicroFacet : public Material {
+private:
+    // The colour of the diffuse material
+    const Colour albedo;
+    // Material parameters, dependant on the roughness of the material
+    const double A, B;
+
+    // Helper function to construct the A parameter
+    constexpr static double construct_A(double sigma) noexcept {
+        const double variance = sigma * sigma;
+        return 1.0 - variance / (2.0 * (variance + 0.33));
+    }
+
+    // Helper function to construct the B parameter
+    constexpr static double construct_B(double sigma) noexcept {
+        const double variance = sigma * sigma;
+        return 0.45 * variance / (variance + 0.09);
+    }
+
+public:
+    // Construct a new MicroFacet material. "sigma" is the standard deviation of
+    // the microfacet angle, in degrees
+    constexpr MicroFacet(Colour albedo, const double sigma) noexcept
+        : albedo(albedo), A(construct_A(utils::to_radians(sigma))),
+          B(construct_B(utils::to_radians(sigma))) {}
 
     // Virtual function override
     virtual ScatterType
--- src/materials/diffuse.cpp
+++ src/materials/diffuse.cpp
@@ -4,20 +4,40 @@
 #include <utils/pdf.hpp>
 #include <utils/vec3.hpp>
 
-Diffuse::ScatterType Diffuse::scatter(const HitRecord & hit_record,
-                                      const Ray & ray_in,
-                                      ScatterRecord & scatter) const noexcept {
+Lambertian::ScatterType
+Lambertian::scatter(const HitRecord & hit_record,
+                    const Ray & ray_in,
+                    ScatterRecord & scatter) const noexcept {
     scatter.attenuation = albedo;
     scatter.is_specular = false;
-    scatter.pdf =
-        std::make_unique<CosinePdf>(CosinePdf(hit_record.surface_normal));
+    scatter.pdf = std::make_unique<CosinePdf>(hit_record.surface_normal);
 
     return ScatterType::Bounce;
 }
 
-double Diffuse::scattering_pdf(const HitRecord & hit_record,
-                               const Ray & scattered_ray) const noexcept {
+double Lambertian::scattering_pdf(const HitRecord & hit_record,
+                                  const Ray & scattered_ray) const noexcept {
     const double cos_theta =
-        scattered_ray.direction.unit_vector().dot(hit_record.surface_normal);
+        scattered_ray.direction.dot(hit_record.surface_normal);
+    return cos_theta < utils::EPSILON ? 0.0 : cos_theta / utils::PI;
+}
+
+MicroFacet::ScatterType
+MicroFacet::scatter(const HitRecord & hit_record,
+                    const Ray & ray_in,
+                    ScatterRecord & scatter) const noexcept {
+    scatter.attenuation = albedo;
+    scatter.is_specular = false;
+    scatter.pdf = std::make_unique<OrenNayar>(
+        hit_record.surface_normal, ray_in.direction.unit_vector(), A, B);
+
+    return ScatterType::Bounce;
+}
+
+double MicroFacet::scattering_pdf(const HitRecord & hit_record,
+                                  const Ray & scattered_ray) const noexcept {
+    const double cos_theta =
+        scattered_ray.direction.dot(hit_record.surface_normal);
+
     return cos_theta < utils::EPSILON ? 0.0 : cos_theta / utils::PI;
 }
