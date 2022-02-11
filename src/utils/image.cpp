e65d59c1516156c57869363c7539a8ae87380452
AldanTanneo <aldantanneo@gmail.com>
2022-02-11 14:22:44 +0100

    made image writing parallel

--- src/utils/image.cpp
+++ src/utils/image.cpp
@@ -1,4 +1,6 @@
 // From src/include
+#include <omp.h>
+#include <stdint.h>
 #include <utils/image.hpp>
 
 // External PNG library
@@ -10,12 +12,15 @@ void Image::save_png(const char * filename) const {
         throw "Could not write image to PNG: Data size mismatch";
     }
     std::vector<uint8_t> raw_data(width * height * 3);
+
+#pragma omp parallel for
     for (size_t i = 0; i < width * height; ++i) {
         Colour c = data[i];
         raw_data[3 * i] = c.red();
         raw_data[3 * i + 1] = c.green();
         raw_data[3 * i + 2] = c.blue();
     }
+
     if (!stbi_write_png(filename, width, height, 3, raw_data.data(),
                         width * 3)) {
         throw "Could not save png image!";
