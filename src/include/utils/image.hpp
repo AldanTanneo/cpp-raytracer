4b1a52c22649dd66f919c09cc6970e3bb95c6f01
AldanTanneo <aldantanneo@gmail.com>
2022-02-11 14:20:19 +0100

    various fixes

--- src/include/utils/image.hpp
+++ src/include/utils/image.hpp
@@ -11,18 +11,19 @@
 
 namespace kernels {
     // Identity kernel
-    constexpr double const identity[9] = {0, 0, 0, 0, 1, 0, 0, 0, 0};
+    static constexpr double const identity[9] = {0, 0, 0, 0, 1, 0, 0, 0, 0};
     // Uniform blur kernel
-    constexpr double const box_blur[9] = {1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0,
-                                          1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0,
-                                          1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0};
+    static constexpr double const box_blur[9] = {
+        1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0,
+        1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0};
     // Gaussian blur kernel
-    constexpr double const gaussian_blur[9] = {
+    static constexpr double const gaussian_blur[9] = {
         0.0625, 0.125, 0.0625, 0.125, 0.25, 0.125, 0.0625, 0.125, 0.0625};
     // Sharpening kernel
-    constexpr double const sharpen[9] = {0, -0.5, 0, -0.5, 3, -0.5, 0, -0.5, 0};
+    static constexpr double const sharpen[9] = {0,    -0.5, 0,    -0.5, 3,
+                                                -0.5, 0,    -0.5, 0};
     // Embossing kernel
-    constexpr double const emboss[9] = {-2, -1, 0, -1, 1, 1, 0, 1, 2};
+    static constexpr double const emboss[9] = {-2, -1, 0, -1, 1, 1, 0, 1, 2};
 } // namespace kernels
 
 // Class for writing and reading PPM images. See the PPM specification at
@@ -35,31 +36,34 @@ public:
 private:
     // A vector of pixels
     container data;
-    // Size of the image
-    const size_t width, height;
 
 public:
+    // Size of the image
+    const int64_t width, height;
+
     // Create an empty image of size width * height
-    inline Image(size_t width, size_t height) noexcept
+    inline Image(int64_t width, int64_t height) noexcept
         : data(container(width * height)), width(width), height(height) {}
     // Create an image of size width * height, using the pixels in the given
     // vector
-    inline Image(container & data, size_t width, size_t height)
+    inline Image(container & data, int64_t width, int64_t height)
         : data(data), width(width), height(height) {
         if (data.size() > width * height) {
             throw "Could not construct Image: too many pixels in the given "
                   "vector.";
+        } else {
+            data.resize(width * height);
         }
     }
     // Create a black image of size width * height
-    inline static Image black(size_t width, size_t height) noexcept {
+    inline static Image black(int64_t width, int64_t height) noexcept {
         return Image(width, height);
     }
 
     // Parse a PPM image from a stream
     template <class charT, class charTraits = std::char_traits<charT>>
     static Image decode_ppm_stream(std::basic_istream<charT, charTraits> & is) {
-        size_t width, height;
+        int64_t width, height;
         char buffer[8];
         uint16_t maxval;
 
@@ -143,13 +147,13 @@ public:
     }
 
     // Get the image width
-    constexpr uint64_t get_width() const noexcept { return width; }
+    constexpr int64_t get_width() const noexcept { return width; }
     // Get the image height
-    constexpr uint64_t get_height() const noexcept { return height; }
+    constexpr int64_t get_height() const noexcept { return height; }
     // Get the image size
-    inline uint64_t size() const noexcept { return data.size(); }
+    inline int64_t size() const noexcept { return data.size(); }
     // Get the image container capacity
-    inline uint64_t capacity() const noexcept { return data.capacity(); }
+    inline int64_t capacity() const noexcept { return data.capacity(); }
 
     // Const vector access
     inline const Colour & operator[](size_t index) const noexcept {
@@ -157,6 +161,13 @@ public:
     }
     // Index the image by indexing the underlying vector
     inline Colour & operator[](size_t index) noexcept { return data[index]; }
+    // Index the image by indexing the image matrix
+    inline const Colour & at(size_t i, size_t j) const noexcept {
+        return data[i * width + j];
+    }
+    inline Colour & at(size_t i, size_t j) noexcept {
+        return data[i * width + j];
+    }
 
     // Append a pixel to the image
     inline void push(Colour pixel) noexcept { data.push_back(pixel); }
@@ -211,8 +222,8 @@ public:
             return false;
         }
 
-        uint64_t data_size = size();
-        for (uint64_t i = 0; i < data_size; ++i) {
+        int64_t data_size = size();
+        for (int64_t i = 0; i < data_size; ++i) {
             if (data[i] != other.data[i]) {
                 return false;
             }
@@ -242,8 +253,8 @@ public:
         container current_line_data(width);
 
         // Store the current line
-        for (size_t i = 0; i < width; ++i)
-            current_line_data[i] = data[i];
+        for (int64_t j = 0; j < width; ++j)
+            current_line_data[j] = data[j];
 
         // Top left corner
         data[0] = (kernel[0] + kernel[1] + kernel[3] + kernel[4])
@@ -259,56 +270,56 @@ public:
                           + (kernel[7] + kernel[8]) * data[2 * width - 1];
 
         // Top row
-        for (size_t i = 1; i < width - 1; ++i) {
-            data[i] = (kernel[0] + kernel[3]) * current_line_data[i - 1]
-                      + (kernel[1] + kernel[4]) * current_line_data[i]
-                      + (kernel[2] + kernel[5]) * current_line_data[i + 1]
-                      + kernel[6] * data[width + i - 1]
-                      + kernel[7] * data[width + i]
-                      + kernel[8] * data[width + i + 1];
+        for (int64_t j = 1; j < width - 1; ++j) {
+            data[j] = (kernel[0] + kernel[3]) * current_line_data[j - 1]
+                      + (kernel[1] + kernel[4]) * current_line_data[j]
+                      + (kernel[2] + kernel[5]) * current_line_data[j + 1]
+                      + kernel[6] * data[width + j - 1]
+                      + kernel[7] * data[width + j]
+                      + kernel[8] * data[width + j + 1];
         }
 
         // Put current_line_data in old_line_data
         old_line_data.swap(current_line_data);
 
-        for (size_t j = 1; j < height - 1; ++j) {
-            for (size_t i = 0; i < width; ++i)
-                current_line_data[i] = data[j * width + i];
+        for (int64_t i = 1; i < height - 1; ++i) {
+            for (int64_t j = 0; j < width; ++j)
+                current_line_data[j] = data[i * width + j];
 
             // Left column
-            data[j * width] = (kernel[0] + kernel[1]) * old_line_data[0]
+            data[i * width] = (kernel[0] + kernel[1]) * old_line_data[0]
                               + kernel[2] * old_line_data[1]
                               + (kernel[3] + kernel[4]) * current_line_data[0]
                               + kernel[5] * current_line_data[1]
-                              + (kernel[6] + kernel[7]) * data[(j + 1) * width]
-                              + kernel[8] * data[(j + 1) * width + 1];
+                              + (kernel[6] + kernel[7]) * data[(i + 1) * width]
+                              + kernel[8] * data[(i + 1) * width + 1];
             // Right column
-            data[(j + 1) * width - 1] =
+            data[(i + 1) * width - 1] =
                 kernel[0] * old_line_data[width - 2]
                 + (kernel[1] + kernel[2]) * old_line_data[width - 1]
                 + kernel[3] * current_line_data[width - 2]
                 + (kernel[4] + kernel[5]) * current_line_data[width - 1]
-                + kernel[6] * data[(j + 2) * width - 2]
-                + (kernel[7] + kernel[8]) * data[(j + 2) * width - 1];
+                + kernel[6] * data[(i + 2) * width - 2]
+                + (kernel[7] + kernel[8]) * data[(i + 2) * width - 1];
 
-            for (size_t i = 1; i < width - 1; ++i) {
-                data[j * width + i] =
-                    kernel[0] * old_line_data[i - 1]
-                    + kernel[1] * old_line_data[i]
-                    + kernel[2] * old_line_data[i + 1]
-                    + kernel[3] * current_line_data[i - 1]
-                    + kernel[4] * current_line_data[i]
-                    + kernel[5] * current_line_data[i + 1]
-                    + kernel[6] * data[(j + 1) * width + i - 1]
-                    + kernel[7] * data[(j + 1) * width + i]
-                    + kernel[8] * data[(j + 1) * width + i + 1];
+            for (int64_t j = 1; j < width - 1; ++j) {
+                data[i * width + j] =
+                    kernel[0] * old_line_data[j - 1]
+                    + kernel[1] * old_line_data[j]
+                    + kernel[2] * old_line_data[j + 1]
+                    + kernel[3] * current_line_data[j - 1]
+                    + kernel[4] * current_line_data[j]
+                    + kernel[5] * current_line_data[j + 1]
+                    + kernel[6] * data[(i + 1) * width + j - 1]
+                    + kernel[7] * data[(i + 1) * width + j]
+                    + kernel[8] * data[(i + 1) * width + j + 1];
             }
 
             old_line_data.swap(current_line_data);
         }
 
-        for (size_t i = 0; i < width; ++i)
-            current_line_data[i] = data[(height - 1) * width + i];
+        for (int64_t j = 0; j < width; ++j)
+            current_line_data[j] = data[(height - 1) * width + j];
 
         // Bottom left corner
         data[(height - 1) * width] =
@@ -326,13 +337,13 @@ public:
                   * current_line_data[width - 1];
 
         // Bottom row
-        for (size_t i = 1; i < width - 1; ++i) {
-            data[(height - 1) * width + i] =
-                kernel[0] * old_line_data[i - 1] + kernel[1] * old_line_data[i]
-                + kernel[2] * old_line_data[i + 1]
-                + (kernel[3] + kernel[6]) * current_line_data[i - 1]
-                + (kernel[4] + kernel[7]) * current_line_data[i]
-                + (kernel[5] + kernel[8]) * current_line_data[i + 1];
+        for (int64_t j = 1; j < width - 1; ++j) {
+            data[(height - 1) * width + j] =
+                kernel[0] * old_line_data[j - 1] + kernel[1] * old_line_data[j]
+                + kernel[2] * old_line_data[j + 1]
+                + (kernel[3] + kernel[6]) * current_line_data[j - 1]
+                + (kernel[4] + kernel[7]) * current_line_data[j]
+                + (kernel[5] + kernel[8]) * current_line_data[j + 1];
         }
     }
 
